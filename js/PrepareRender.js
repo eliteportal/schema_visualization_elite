a787917ff90c0b7979b3561b5417d4c8b1160354
Nicholas Lee <nicholas.lee@sagebase.org>
2023-11-16 18:05:49 -0800

    created dropdown

--- js/PrepareRender.js
+++ js/PrepareRender.js
@@ -1,27 +1,24 @@
-$.get('files/config.yml').done(function (data) {
-    //loading content of the config file
-    var config_content = jsyaml.load(data)
+$.get("files/config.yml").done(function (data) {
+  //loading content of the config file
+  // var config_content = jsyaml.load(data);
 
-    //get schema url 
-    var schema_url = config_content["schema_url"]
+  var dropdown = document.getElementById("selection");
+  var schema_url = dropdown.value.toString();
 
+  dropdown.addEventListener("change", function () {
+    schema_url = dropdown.value.toString();
     //////////////////for using schematic API
-    // getRequestedJson(schema_url).then(tangled_tree_data => {
-    //     var chart_dta = chart(tangled_tree_data);
-    //     createCollapsibleTree(chart_dta, schema_url)
-    // })
+    getRequestedJson(schema_url).then((tangled_tree_data) => {
+      var chart_dta = chart(tangled_tree_data);
+      createCollapsibleTree(chart_dta, schema_url);
+    });
+  });
 
-    //////////////////for using static files
-    // if (schema == "HTAN") {
-    var tangled_tree_data = parseJSON('files/JSON/ELITE-tangled-tree.json');
-    // }
+  console.log(schema_url);
 
-    tangled_tree_data.then(tangled_tree_dta => {
-        //get tangle tree layout
-        var chart_dta = chart(tangled_tree_dta);
-
-        //draw collapsible tree
-        createCollapsibleTree(chart_dta, schema_url);
-    })
-
-})
+  //////////////////for using schematic API
+  // getRequestedJson(schema_url).then((tangled_tree_data) => {
+  //   var chart_dta = chart(tangled_tree_data);
+  //   createCollapsibleTree(chart_dta, schema_url);
+  // });
+});
